7cfb2ba1a76074fa50d84893807c7b79546246c6
Jasper Goens <jasper_goens@hotmail.com>
2024-09-02 01:33:10 +0200

    add Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import './global.css';
 import Link from "next/link";
 import React from "react";
 import localFont from 'next/font/local';
+import Image from 'next/image';
+
+import TarragonTiny from "../public/images/Tarragon_Tiny.svg";
+import TarragonTitle from "../public/images/Tarragon_Title.svg";
+import CDLogo from "../public/images/CD_Logo.svg";
 
 // Import local Fonts
 const Rockwell = localFont({src: './fonts/Rockwell.woff2' })
@@ -23,16 +28,20 @@ export default async function RootLayout({
         }}
       >
         <header>
-          <Link href="/">Home</Link>
-          {" | "}
-          <Link href="/posts">Posts</Link>
-          <h1>H1</h1>
-          <h2>H2</h2>
-          <h3>H3</h3>
-          <h4>H4</h4>
-          <b>B</b><br/>
-          <i>I</i><br/>
-          <a>A</a><br/>
+          <Link href="/"><div style={{'display':'flex', 'alignItems':'center'}}>
+            <Image
+                src={TarragonTiny}
+                alt="Tarragon Logo"
+            />
+            <Image
+                src={TarragonTitle}
+                alt="Tarragon Text"
+                className="TarragonTitle"
+            />
+          </div></Link>
+          <Link href="/#Events"><h1>Events</h1></Link>
+          <Link href="/CD"><Image src={CDLogo} alt="Crossing Dimensions Logo"/></Link>
+          <Link href="https://discord.com/invite/TjDUu2Gkag"><h1>Discord</h1></Link>
         </header>
         <main>{children}</main>
       </body>
