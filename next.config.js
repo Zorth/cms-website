39e42a0c71e15edbb3e609287d17b91ec69c2740
Jasper Goens <jasper_goens@hotmail.com>
2024-09-09 19:26:46 +0200

    add image domain to nextjs module export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+    images: {
+        domains: ['assets.tina.io'],
+    },
   async rewrites() {
     return [
       // {
--- tina/config.js
+++ tina/config.js
@@ -5,31 +5,32 @@ import dragon from "./collections/dragon";
 import sponsor from "./collections/sponsor";
 
 export const config = defineConfig({
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
-  token: process.env.TINA_TOKEN,
-  media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
-    tina: {
-      publicFolder: "public",
-      mediaRoot: "uploads",
+    clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+    branch:
+        process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+        process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+        process.env.HEAD, // Netlify branch env
+    token: process.env.TINA_TOKEN,
+    media: {
+        // If you wanted cloudinary do this
+        // loadCustomStore: async () => {
+        //   const pack = await import("next-tinacms-cloudinary");
+        //   return pack.TinaCloudCloudinaryMediaStore;
+        // },
+        // this is the config for the tina cloud media store
+        tina: {
+            publicFolder: "public",
+            mediaRoot: "uploads",
+            static: false,
+        },
     },
-  },
-  build: {
-    publicFolder: "public", // The public asset folder for your framework
-    outputFolder: "admin", // within the public folder
-  },
-  schema: {
-    collections: [page, event, dragon, sponsor],
-  },
+    build: {
+        publicFolder: "public", // The public asset folder for your framework
+        outputFolder: "admin", // within the public folder
+    },
+    schema: {
+        collections: [page, event, dragon, sponsor],
+    },
     search: {
         tina: {
             indexerToken: 'c336d6a3b8ca5a68a460132ee55e30c5ca75b6f4',
