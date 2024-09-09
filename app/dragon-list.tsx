8162156af86118608d6f3416c0cfa30cfa0d91a0
Jasper Goens <jasper_goens@hotmail.com>
2024-09-09 14:27:38 +0200

    take Dragon signup out of carousel

--- app/dragon-list.tsx
+++ app/dragon-list.tsx
@@ -10,17 +10,19 @@ export default function DragonList(props) {
     const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
     return (
-        <div className="dragon-list" ref={emblaRef}>
-            <div className="dragon-list-container">
-                {props.data.dragonConnection.edges
-                    .map((dragon) => (DragonSnippet(dragon)))
-                }
-                <div className="dragon-snippet-container"><Link href={"/404"} className="dragon-snippet" style={{ background: 'var(--primary_dark)' }}>
-                    <h1>Want to become a dragon?</h1>
-                    <p>Click here!</p>
-                </Link></div>
+        <>
+            <div className="dragon-list" ref={emblaRef}>
+                <div className="dragon-list-container">
+                    {props.data.dragonConnection.edges
+                        .map((dragon) => (DragonSnippet(dragon)))
+                    }
+                </div>
             </div>
-        </div>
+            <Link href={"/404"} className="dragon-snippet" style={{ background: 'var(--primary_dark)' }}>
+                <h1>Want to become a dragon?</h1>
+                <p>Click here!</p>
+            </Link>
+        </>
     )
 }
 
@@ -28,19 +30,19 @@ function DragonSnippet(dragon) {
     return (
         <div id={dragon.node.id} className="dragon-snippet-container">
             <div className="dragon-snippet">
-                {dragon.node.image? <Image
+                {dragon.node.image ? <Image
                     src={dragon.node.image}
                     alt={dragon.node.name}
                     width={500}
                     height={500}
                     className="dragon-image"
-                    /> : <Image
+                /> : <Image
                     src={TarragonLogo}
                     alt={dragon.node.name}
                     width={500}
                     height={500}
                     className="dragon-image"
-                    />}
+                />}
                 <h1>{dragon.node.name}</h1>
                 <h2>{dragon.node.title}</h2>
             </div>
