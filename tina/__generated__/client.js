4faaa445f741a1b7f737a36a832322ffd421c4b0
Zorth Thorch <jasper_goens@hotmail.com>
2026-04-09 21:55:50 +0200

    rem generated

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,4 +1,4 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: "/home/zorth/Projects/cms-website/tina/__generated__/.cache/1775760737255", url: "http://localhost:4001/graphql", token: "b9ae3ceb7bc881c73de5d4884b448b2da5abf569", queries });
+export const client = createClient({ cacheDir: "/home/zorth/Projects/cms-website/tina/__generated__/.cache/1775763865961", url: "http://localhost:4001/graphql", token: "b9ae3ceb7bc881c73de5d4884b448b2da5abf569", queries });
 export default client;
